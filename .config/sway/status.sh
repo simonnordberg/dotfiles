b41e20d6634dcf810f21bce552a5ff13f3ccd004
Simon Nordberg <simon@simonnordberg.com>
2023-09-06 09:39:59 +0200

    Add keyboard switch

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -4,4 +4,18 @@ date_formatted=$(date +'%Y-%m-%d %H:%M')
 battery_info=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "state|percentage" | awk '{print $2}')
 audio_info=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}')
 
-echo $audio_info 🔉 $battery_info 🔋 $date_formatted
+# Keyboard layout
+layout=$(swaymsg -t get_inputs | jq -r '.[0].xkb_active_layout_name')
+case $layout in
+    Swedish)
+        layout_icon="🇸🇪"
+        ;;
+    English*)
+        layout_icon="🇺🇸"
+        ;;
+    *)
+        layout_icon="❓"
+        ;;
+esac
+
+echo $audio_info 🔉 $battery_info 🔋 $date_formatted $layout_icon
