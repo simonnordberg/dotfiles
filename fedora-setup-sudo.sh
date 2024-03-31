41c6e923809f7f4c25a2891fffe732499d9a9174
Simon Nordberg <simon@simonnordberg.com>
2024-03-31 12:56:56 +0200

    Updates

--- fedora-setup-sudo.sh
+++ fedora-setup-sudo.sh
@@ -48,6 +48,8 @@ PARTIAL_FSTAB="$SCRIPT_DIR/root/etc/fstab.partial"
 START_MARKER="# Begin Custom Mounts"
 END_MARKER="# End Custom Mounts"
 
+sed -i "/$START_MARKER/,/$END_MARKER/d" /etc/fstab
+
 {
     echo "$START_MARKER"
     cat "$PARTIAL_FSTAB"
--- ubuntu-setup-sudo.sh
+++ ubuntu-setup-sudo.sh
@@ -68,6 +68,20 @@ ln -fsn $SCRIPT_DIR/root/etc/systemd/sleep.conf /etc/systemd/sleep.conf
 ln -fsn $SCRIPT_DIR/root/etc/systemd/logind.conf /etc/systemd/logind.conf
 ln -fsn $SCRIPT_DIR/root/usr/local/bin/ssway /usr/local/bin/ssway
 
+echo "Configuring fstab"
+cp /etc/fstab "/etc/fstab.backup.$(date +%Y%m%d%H%M%S)"
+PARTIAL_FSTAB="$SCRIPT_DIR/root/etc/fstab.partial"
+START_MARKER="# Begin Custom Mounts"
+END_MARKER="# End Custom Mounts"
+
+sed -i "/$START_MARKER/,/$END_MARKER/d" /etc/fstab
+
+{
+    echo "$START_MARKER"
+    cat "$PARTIAL_FSTAB"
+    echo "$END_MARKER"
+} >> /etc/fstab
+
 mem=$(awk '/MemTotal/{ print $2 }' /proc/meminfo)
 swap=$(awk '/partition/{ print $3 }' /proc/swaps)
 
