5998c3a40d09ffd4eeb432c5da517adad8106d5b
Simon Nordberg <simon@simonnordberg.com>
2024-07-05 15:33:18 +0200

    Use j4-dmenu-desktop

--- fedora-setup-sudo.sh
+++ fedora-setup-sudo.sh
@@ -44,7 +44,8 @@ dnf install -y \
     1password \
     slurp \
     alacritty \
-    bemenu
+    bemenu \
+    j4-dmenu-desktop
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
