c177622ba897ea47a77f43f95d794dc88c3d3686
Simon Nordberg <simon@simonnordberg.com>
2024-07-09 16:36:32 +0200

    Add Sway wrapper to enable debug

--- install/10-desktop/10-sway.sh
+++ install/10-desktop/10-sway.sh
@@ -16,3 +16,6 @@ mkdir -p $HOME/.config
 ln -fsn $SCRIPT_DIR/.config/sway $HOME/.config/sway
 ln -fsn $SCRIPT_DIR/.config/waybar $HOME/.config/waybar
 ln -fsn $SCRIPT_DIR/.config/mako $HOME/.config/mako
+
+sudo cp $SCRIPT_DIR/root/usr/share/wayland-sessions/sway-wrapper.desktop /usr/share/wayland-sessions/sway-wrapper.desktop
+sudo cp $SCRIPT_DIR/root/usr/local/bin/sway-wrapper /usr/local/bin/sway-wrapper
