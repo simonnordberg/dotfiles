83b928a777c6658d5cae753184b470329304da8a
Simon Nordberg <simon@simonnordberg.com>
2024-07-09 16:21:24 +0200

    Update

--- install/10-desktop/10-sway.sh
+++ install/10-desktop/10-sway.sh
@@ -15,3 +15,4 @@ sudo dnf install -y \
 mkdir -p $HOME/.config
 ln -fsn $SCRIPT_DIR/.config/sway $HOME/.config/sway
 ln -fsn $SCRIPT_DIR/.config/waybar $HOME/.config/waybar
+ln -fsn $SCRIPT_DIR/.config/mako $HOME/.config/mako
