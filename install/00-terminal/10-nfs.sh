9d46eec89c42348eafd0c36504ce181c7fbd95b7
Simon Nordberg <simon@simonnordberg.com>
2024-07-12 10:47:10 +0200

    Refactor nfs mounts

--- install/00-terminal/10-nfs.sh
+++ install/00-terminal/10-nfs.sh
@@ -1,12 +1,13 @@
-PARTIAL_FSTAB="$SCRIPT_DIR/root/etc/fstab.partial"
-START_MARKER="# Begin Custom Mounts"
-END_MARKER="# End Custom Mounts"
+PARTIAL="$SCRIPT_DIR/root/etc/fstab.partial"
+START_MARKER="# start-fstab"
+END_MARKER="# end-fstab"
 
 sudo dnf install -y nfs-utils
 sudo cp /etc/fstab "/etc/fstab.backup.$(date +%Y%m%d%H%M%S)"
 sudo sed -i "/$START_MARKER/,/$END_MARKER/d" /etc/fstab
 {
   echo "$START_MARKER"
-  cat "$PARTIAL_FSTAB"
+  cat "$PARTIAL"
   echo "$END_MARKER"
-} | sudo tee -a /etc/fstab > /dev/null
+} | sudo tee -a /etc/fstab >/dev/null
+
