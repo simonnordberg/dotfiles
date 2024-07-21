868fd51d1b2aee07060c90263956f7f5c6733864
Simon Nordberg <simon@simonnordberg.com>
2024-07-21 14:51:13 +0200

    Fix routes for multiple networks

--- install/00-terminal/05-route.sh
+++ install/00-terminal/05-route.sh
@@ -1,17 +1,18 @@
-INTERFACE="enp195s0f4u1u6"
-DESTINATION="192.168.5.0/24"
-GATEWAY="192.168.2.1"
+connections=("Mumindalen 24/7" "enp195s0f4u1u6")
+destination="192.168.5.0/24"
+gateway="192.168.2.1"
 
 check_route() {
-	nmcli connection show "$INTERFACE" | grep -w "$DESTINATION" >/dev/null 2>&1
+  nmcli connection show "$1" | grep -w "$destination" >/dev/null 2>&1
 }
 
-# Main script logic
-if check_route; then
-	echo "Route $DESTINATION via $GATEWAY already exists for connection $INTERFACE."
-else
-	nmcli connection modify "$INTERFACE" +ipv4.routes "$DESTINATION $GATEWAY"
-	nmcli connection down "$INTERFACE"
-	nmcli connection up "$INTERFACE"
-	echo "Added route $DESTINATION via $GATEWAY to connection $INTERFACE."
-fi
+for connection in "${connections[@]}"; do
+  if check_route "$connection"; then
+    echo "Route $destination via $gateway already exists for connection $connection."
+  else
+    nmcli connection modify "$connection" +ipv4.routes "$destination $gateway"
+    nmcli connection down "$connection"
+    nmcli connection up "$connection"
+    echo "Added route $destination via $gateway to connection $connection."
+  fi
+done
