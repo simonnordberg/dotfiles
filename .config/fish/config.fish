daca2128135687bc36f00c9668c006940ebfc54f
Simon Nordberg <simon@simonnordberg.com>
2024-08-16 16:37:57 +0200

    Fix pyenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
 set -U fish_greeting
 
 starship init fish | source
+
+set -Ux PYENV_ROOT $HOME/.pyenv
+fish_add_path $PYENV_ROOT/bin
+pyenv init - | source
--- install/10-pyenv.sh
+++ install/10-pyenv.sh
@@ -1,4 +1,8 @@
 VERSION=3.12.4
 
-[ ! -e $HOME/.pyenv ] && curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+PATH=$PATH:$HOME/.pyenv/bin
+
+[ -e $HOME/.pyenv ] && rm -rf $HOME/.pyenv
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+
 pyenv install --skip-existing $VERSION
