932dfa80379c89effd78db535f5434b00f000c8e
Simon Nordberg <simon@simonnordberg.com>
2024-09-12 16:03:57 +0200

    Add LS_COLORS

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,5 @@ starship init fish | source
 set -Ux PYENV_ROOT $HOME/.pyenv
 fish_add_path $PYENV_ROOT/bin
 pyenv init - | source
+
+set -Ux LS_COLORS "di=0;36:ln=0;35:so=0;33:pi=0;33:ex=0;32:bd=1;33;01:cd=1;33;01:su=0;37:sg=0;30:tw=0;37:ow=0;34:st=0;37:*.tar=1;31:*.gz=1;31:*.zip=1;31:*.bz2=1;31:*.xz=1;31"
