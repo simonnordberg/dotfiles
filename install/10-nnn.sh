b987840c9ecbf1f1caae39c44ef40779e8bd9fbf
Simon Nordberg <simon@simonnordberg.com>
2024-11-26 08:53:19 +0100

    Force reinstall nnn plugins

--- install/10-nnn.sh
+++ install/10-nnn.sh
@@ -1,2 +1,7 @@
 sudo dnf install -y nnn
+
+if [ -d $HOME/.config/nnn/plugins ]; then
+  rm -rf $HOME/.config/nnn/plugins
+fi
+
 sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
