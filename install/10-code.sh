192796124b88241c7bb34a8e7e318051430a8d82
Simon Nordberg <simon@simonnordberg.com>
2025-01-07 11:18:00 +0100

    Install vscode

---
+++ install/10-code.sh
@@ -0,0 +1,10 @@
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo tee /etc/yum.repos.d/vscode.repo >/dev/null <<EOF
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+sudo dnf install -y code
