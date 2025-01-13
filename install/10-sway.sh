bf5642a57cacf83628988e0aae7095275151a953
Simon Nordberg <simon@simonnordberg.com>
2025-01-13 21:54:37 +0100

    Multiple monitor setup

--- install/10-sway.sh
+++ install/10-sway.sh
@@ -12,12 +12,14 @@ sudo dnf install -y \
   j4-dmenu-desktop \
   bemenu \
   google-noto-color-emoji-fonts \
-  mako
+  mako \
+  kanshi
 
 mkdir -p $HOME/.config
 ln -fsn $BASE_DIR/.config/sway $HOME/.config/sway
 ln -fsn $BASE_DIR/.config/waybar $HOME/.config/waybar
 ln -fsn $BASE_DIR/.config/mako $HOME/.config/mako
+ln -fsn $BASE_DIR/.config/kanshi $HOME/.config/kanshi
 
 sudo cp $BASE_DIR/root/usr/share/wayland-sessions/sway-wrapper.desktop /usr/share/wayland-sessions/sway-wrapper.desktop
 sudo cp $BASE_DIR/root/usr/local/bin/sway-wrapper /usr/local/bin/sway-wrapper
