7299751d1719f4af74bad30c81e1326f3651da04
Simon Nordberg <simon@simonnordberg.com>
2025-01-13 21:55:04 +0100

    Fix python deps

--- install/10-pyenv.sh
+++ install/10-pyenv.sh
@@ -1,7 +1,11 @@
 VERSION=3.12.4
 PATH=$PATH:$HOME/.pyenv/bin
 
-sudo dnf install -y zlib-ng-compat zlib-ng-compat-devel
+sudo dnf install -y \
+  zlib-ng-compat \
+  zlib-ng-compat-devel \
+  sqlite-devel \
+  libffi-devel
 
 if [ ! -e $HOME/.pyenv ]; then
   curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
