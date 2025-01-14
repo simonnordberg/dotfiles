97031fa3f4bbab9aa92466ac7bb74c459583c7ec
Simon Nordberg <simon@simonnordberg.com>
2025-01-14 22:24:17 +0100

    Install Fira Code next to Hack

--- install/10-fonts.sh
+++ install/10-fonts.sh
@@ -1,5 +1,6 @@
 FONTS=$HOME/.local/share/fonts
 
+sudo dnf install -y fira-code-fonts
 [ -d $FONTS ] && rm -rf $FONTS && echo "Deleted fonts dir: $FONTS"
 mkdir -p $FONTS
 curl -fL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.tar.xz | tar -xJ -C $FONTS
