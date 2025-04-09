ed8659599fb386c0ba944f56e13fd7dd09363365
Simon Nordberg <simon@simonnordberg.com>
2025-04-09 08:23:50 +0200

    remove lang config

--- .config/nvim/lua/plugins/languages.lua
+++
@@ -1,7 +0,0 @@
-return {
-	{ import = "lazyvim.plugins.extras.lang.typescript" },
-	{ import = "lazyvim.plugins.extras.lang.json" },
-	{ import = "lazyvim.plugins.extras.lang.python" },
-	{ import = "lazyvim.plugins.extras.lang.toml" },
-	{ import = "lazyvim.plugins.extras.lang.yaml" },
-}
