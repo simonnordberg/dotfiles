cc769ea5e21bfe4ec501756799bb1eba61ef373e
Simon Nordberg <simon@simonnordberg.com>
2025-04-29 08:53:14 +0200

    Update sway configuration and systemd services for improved power management
    - Adjusted swayidle timeouts for locking and display power management to 5 minutes.
    - Added a new systemd service to reset USB controllers after resume.
    - Introduced a wakeup script to manage ACPI wakeup events for USB devices.

--- install/05-usb.sh
+++ install/05-usb.sh
@@ -1,3 +1,6 @@
 sudo cp $BASE_DIR/root/etc/systemd/system/reset-usb-after-resume.service /etc/systemd/system/reset-usb-after-resume.service
 sudo systemctl daemon-reload
 sudo systemctl enable reset-usb-after-resume.service
+
+sudo cp $BASE_DIR/root/usr/lib/systemd/system-sleep/wakeup /usr/lib/systemd/system-sleep/wakeup
+sudo chmod +x /usr/lib/systemd/system-sleep/wakeup
