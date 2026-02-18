95f57e03049c80d43bc21b7736969872c7e83f57
Simon Nordberg <simon@simonnordberg.com>
2026-02-18 08:13:39 +0100

    chore: remove omarchy

--- omarchy/waybar/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-
-mkdir -p $HOME/.config/waybar
-cp $SCRIPT_DIR/config.jsonc $HOME/.config/waybar/config.jsonc
-
-echo "Waybar customizations installed!"
