5ec016d7a536307b11214be0cb324582046decb6
Simon Nordberg <simon@simonnordberg.com>
2026-02-18 08:11:22 +0100

    feat: add niri

---
+++ services/niri/install.sh
@@ -0,0 +1,9 @@
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+sudo dnf copr enable avengemedia/dms -y
+sudo dnf copr enable yalter/niri -y
+sudo dnf install niri dms
+systemctl --user add-wants niri.service dms
+
+mkdir -p $HOME/.config/niri
+cp $SCRIPT_DIR/config.kdl $HOME/.config/niri/config.kdl
