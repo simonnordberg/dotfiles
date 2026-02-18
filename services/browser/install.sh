d4ba6ec5fe86a2cc550aa1636226e9b270b5d166
Simon Nordberg <simon@simonnordberg.com>
2026-02-18 08:16:23 +0100

    chore: s/brave/chrome/

--- services/browser/install.sh
+++
@@ -1,3 +0,0 @@
-sudo dnf install -y dnf-plugins-core
-sudo dnf config-manager addrepo --overwrite --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-sudo dnf install -y brave-browser
