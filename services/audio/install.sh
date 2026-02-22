20e544533de4d9a32fe93f68db72927f9ce4d21f
Simon Nordberg <simon@simonnordberg.com>
2026-02-22 17:56:09 +0100

    chore: fixes

--- services/audio/install.sh
+++ services/audio/install.sh
@@ -5,3 +5,4 @@ export SERVICE_DIR="$(dirname "$(readlink -f "$0")")"
 rm -rf $HOME/.config/pipewire
 mkdir -p "$HOME/.config/pipewire"
 cp $SERVICE_DIR/pipewire.conf $HOME/.config/pipewire/pipewire.conf
+systemctl --user restart pipewire.service
--- services/ghostty/install.sh
+++ services/ghostty/install.sh
@@ -1,2 +1,6 @@
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
 sudo dnf copr enable scottames/ghostty -y
 sudo dnf install ghostty -y
+
+cp $SCRIPT_DIR/config $HOME/.config/ghostty/config
