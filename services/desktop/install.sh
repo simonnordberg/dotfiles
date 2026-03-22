ddb2bd7145ab43a869b64f61067c46beaae57c42
Simon Nordberg <simon@simonnordberg.com>
2026-03-22 12:16:26 +0100

    Revert "feat: disable suspend on desktop"
    This reverts commit a6cab8ba5997b3322db1353ed6ff020f7c7ac749.

--- services/desktop/install.sh
+++ services/desktop/install.sh
@@ -3,6 +3,3 @@ sudo dnf install -y gdm
 sudo systemctl enable gdm
 sudo systemctl set-default graphical.target
 sudo usermod -a -G video $USER
-
-# Disable suspend
-sudo systemctl mask suspend.target hibernate.target hybrid-sleep.target suspend-then-hibernate.target
