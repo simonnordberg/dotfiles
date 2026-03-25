edad9c40f17f876a0d02849d4549bf9dd62bc05a
Simon Nordberg <simon@simonnordberg.com>
2026-03-25 10:45:49 +0100

    refactor: use DMS lock screen everywhere, remove hyprlock

--- services/niri/install.sh
+++ services/niri/install.sh
@@ -2,12 +2,8 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 sudo dnf copr enable avengemedia/dms -y
 sudo dnf copr enable yalter/niri -y
-sudo dnf copr enable sdegler/hyprland -y
-sudo dnf install niri dms hyprlock -y
+sudo dnf install niri dms -y
 systemctl --user add-wants niri.service dms
 
 mkdir -p $HOME/.config/niri
 cp $SCRIPT_DIR/config.kdl $HOME/.config/niri/config.kdl
-
-mkdir -p $HOME/.config/hypr
-cp $SCRIPT_DIR/hyprlock.conf $HOME/.config/hypr/hyprlock.conf
