a343904de89ff23e2f3888df081c891a51b2ed02
Simon Nordberg <simon@simonnordberg.com>
2026-05-04 12:56:30 +0200

    fix(neovim): disable markdownlint-cli2 diagnostics for markdown files

--- services/neovim/config/lua/plugins/markdown.lua
+++ services/neovim/config/lua/plugins/markdown.lua
@@ -1,3 +1,22 @@
 return {
   { import = "lazyvim.plugins.extras.lang.markdown" },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        markdown = {},
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["markdown"] = { "prettier", "markdown-toc" },
+        ["markdown.mdx"] = { "prettier", "markdown-toc" },
+      },
+    },
+  },
 }
